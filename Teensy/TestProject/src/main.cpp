8fad64febe9f43b815972c1262f4e65c05887349
Aaron Weyer <aaron.weyer@ndsu.edu>
2023-10-21 20:25:35 -0500

    working GPS message collection and parsing

--- Teensy/TestProject/src/main.cpp
+++ Teensy/TestProject/src/main.cpp
@@ -1,12 +1,13 @@
-/* 
-Useful links:
-  https://www.sparkfun.com/datasheets/GPS/NMEA%20Reference%20Manual-Rev2.1-Dec07.pdf
-*/
-
+// Include libraries
 #include <Arduino.h>
+#include "gps.h"
 
+// Define constants
 #define GPS Serial1 // GPS module is connected to serial port 1 on Teensy.
+
+// Define global variables
 String GPSmsg;
+GPSmessage GPSparsed;
 
 void setup() {
   // Initialize serial communication to USB port and GPS module.
@@ -16,11 +17,20 @@ void setup() {
 
 void loop() {
   if (GPS.available() > 0) { // Test if read buffer is not empty.
-    GPSmsg = GPS.readStringUntil(13).trim(); // Read input message
+    GPSmsg = GPS.readStringUntil(13); // Read input message
     GPSmsg.trim(); // Trim out any leading spaces.
     if (GPSmsg.startsWith("$GPRMC")) { // Test if image is RMC message
       Serial.println(GPSmsg);
+      GPSparsed = parseGPSmessage(GPSmsg);
+      Serial.println(GPSparsed.valid);
+      Serial.println(GPSparsed.latitude,5);
+      Serial.println(GPSparsed.longitude,5);
     }
   } 
 }
+
+/* 
+Useful links:
+  https://www.sparkfun.com/datasheets/GPS/NMEA%20Reference%20Manual-Rev2.1-Dec07.pdf
+*/
 
