a63826a465f94825b1349790dfe93dc71f980f7c
Jacob <jacob.koshiol@ndsu.edu>
2024-05-02 08:18:21 -0500

    update

--- interface.m
+++ interface.m
@@ -1,13 +1,16 @@
 clear
 arduino = serialport("COM6", 9600);
 
-j = readline(arduino);
+j = str2num(readline(arduino));
 disp(j);
 pause(0.5);
 
 while true
-    h = readline(arduino);
+    h = str2num(readline(arduino));
     j = [j;h];
+    disp("Longitude: " + j(:, 1));
+    disp("Latitude: " + j(:, 2));
+    disp("Brightness: " + j(:, 3));
     output = lightLevelGradient(j(:,1),j(:,2),j(:,3));
     disp("Brightest Point Coords: " + output(1) + ", " + output(3));
 end
