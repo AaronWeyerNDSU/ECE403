96cdda6108fdfb70fc8c9b44d3c8278ac0d18788
Jacob <jacob.koshiol@ndsu.edu>
2024-05-02 08:19:26 -0500

    update

--- interface.m
+++ interface.m
@@ -8,9 +8,9 @@ pause(0.5);
 while true
     h = str2num(readline(arduino));
     j = [j;h];
-    disp("Longitude: " + j(:, 1));
-    disp("Latitude: " + j(:, 2));
-    disp("Brightness: " + j(:, 3));
+    disp("Longitude: " + h(1));
+    disp("Latitude: " + h(2));
+    disp("Brightness: " + h(3));
     output = lightLevelGradient(j(:,1),j(:,2),j(:,3));
     disp("Brightest Point Coords: " + output(1) + ", " + output(3));
 end
